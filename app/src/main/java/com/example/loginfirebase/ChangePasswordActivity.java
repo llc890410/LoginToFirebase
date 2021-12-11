ca28e9b631bea7eee382f0aada429552810a89e1
MichaelLiu <llc890410@gmail.com>
2021-12-12 00:05:50 +0800

    Change password

--- app/src/main/java/com/example/loginfirebase/ChangePasswordActivity.java
+++ app/src/main/java/com/example/loginfirebase/ChangePasswordActivity.java
@@ -1,15 +1,32 @@
 package com.example.loginfirebase;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class ChangePasswordActivity extends AppCompatActivity {
 
+    private static final String TAG = "ChangePasswordActivity";
+
     EditText etNewPassword,etTypeAgain;
     Button btnSubmit;
+
+    String newPassword;
+    String typeAgain;
+
+    FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +36,36 @@ public class ChangePasswordActivity extends AppCompatActivity {
         etNewPassword = findViewById(R.id.etNewPassword);
         etTypeAgain = findViewById(R.id.etTypeAgain);
         btnSubmit = findViewById(R.id.btnSubmit);
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                newPassword = etNewPassword.getText().toString();
+                typeAgain = etTypeAgain.getText().toString();
+
+                //檢查是否大於6字 是否相等
+
+                if(newPassword.equals(typeAgain)) {
+                    user.updatePassword(newPassword)
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    Toast.makeText(ChangePasswordActivity.this,
+                                            "Change Password Successful",Toast.LENGTH_LONG).show();
+                                    Log.d(TAG, "Change Password Successful");
+                                    Log.d(TAG, "New password:"+newPassword);
+                                }
+                            });
+                }
+
+                Intent intent = new Intent();
+                intent.setClass(ChangePasswordActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/loginfirebase/MainActivity.java
+++ app/src/main/java/com/example/loginfirebase/MainActivity.java
@@ -32,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent();
                 intent.setClass(MainActivity.this, ChangePasswordActivity.class);
+                startActivity(intent);
                 finish();
             }
         });
