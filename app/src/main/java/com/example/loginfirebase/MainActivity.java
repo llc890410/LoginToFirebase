60b71213fce4df09fb101da54ede1936dcd9f18b
MichaelLiu <llc890410@gmail.com>
2021-12-11 23:19:07 +0800

    MainActivity get username(email)

--- app/src/main/java/com/example/loginfirebase/MainActivity.java
+++ app/src/main/java/com/example/loginfirebase/MainActivity.java
@@ -4,16 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView tvUsername;
     Button btnChangePassword;
+    String email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        tvUsername = findViewById(R.id.tvUsername);
         btnChangePassword = findViewById(R.id.btnChangePassword);
+
+        Bundle bundle = getIntent().getExtras();
+        email = bundle.getString("email");
+
+        tvUsername.setText(email);
+
     }
 }
