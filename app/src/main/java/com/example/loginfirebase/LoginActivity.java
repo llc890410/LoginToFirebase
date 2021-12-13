8ab08941520b95f02e2cbcefa9d0169f32ba5a88
MichaelLiu <llc890410@gmail.com>
2021-12-14 04:43:47 +0800

    handle login fail exception

--- app/src/main/java/com/example/loginfirebase/LoginActivity.java
+++ app/src/main/java/com/example/loginfirebase/LoginActivity.java
@@ -12,10 +12,15 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
+
+import java.util.Objects;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -43,24 +48,43 @@ public class LoginActivity extends AppCompatActivity {
                 String email = etEmail.getText().toString();
                 String password = etPassword.getText().toString();
 
-                //Login
-                mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        Toast.makeText(LoginActivity.this,"Login Successful",Toast.LENGTH_SHORT).show();
-                        Log.d(TAG,"Login Successful");
+                if (email.isEmpty() || password.isEmpty()) {
+                    Toast.makeText(LoginActivity.this,"email及密碼不可為空",Toast.LENGTH_SHORT).show();
+                } else {
+                    //Login
+                    mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (!task.isSuccessful()) {
+                                String errorCode = ((FirebaseAuthException) Objects.requireNonNull(task.getException())).getErrorCode();
+                                Log.e(TAG, errorCode);
+                                switch (errorCode) {
+                                    case "ERROR_USER_NOT_FOUND":
+                                        Toast.makeText(LoginActivity.this,"沒有此使用者",Toast.LENGTH_SHORT).show();
+                                        break;
+                                    case "ERROR_INVALID_EMAIL":
+                                        Toast.makeText(LoginActivity.this,"email格式錯誤",Toast.LENGTH_SHORT).show();
+                                        break;
+                                    case "ERROR_WRONG_PASSWORD":
+                                        Toast.makeText(LoginActivity.this,"密碼錯誤",Toast.LENGTH_SHORT).show();
+                                        break;
+                                }
+                            } else {
+                                Toast.makeText(LoginActivity.this,"Login Successful",Toast.LENGTH_SHORT).show();
+                                Log.d(TAG,"Login Successful");
 
-                        //switch to MainActivity
-                        Intent intent = new Intent();
-                        intent.setClass(LoginActivity.this, MainActivity.class);
-                        Bundle bundle = new Bundle();
-                        bundle.putString("email",email);
-                        intent.putExtras(bundle);
-                        startActivity(intent);
-                        finish();
-                    }
-                });
-
+                                //switch to MainActivity
+                                Intent intent = new Intent();
+                                intent.setClass(LoginActivity.this, MainActivity.class);
+                                Bundle bundle = new Bundle();
+                                bundle.putString("email",email);
+                                intent.putExtras(bundle);
+                                startActivity(intent);
+                                finish();
+                            }
+                        }
+                    });
+                }
             }
         });
 
